2d3b0090c7a29ee3996c5befbc4e42c24cf3c9e5
savithamk <80167929+savithamk@users.noreply.github.com>
2022-04-04 00:48:19 -0400

    Designed todo page and implemented post method for saving todo.

--- Web-1001-DB-Todo-App/Pages/Index.cshtml.cs
+++ Web-1001-DB-Todo-App/Pages/Index.cshtml.cs
@@ -5,21 +5,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Web_1001_DB_Todo_Context;
+using Web_1001_DB_Todo_Models;
 
 namespace Web_1001_DB_Todo_App.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly TodoContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [FromForm]
+        public Todo todo { get; set; }
+        public List<Todo> Todos { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, TodoContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
-
+            var todoQuery = _db.Todos.Select(todo => todo);
+            Todos = todoQuery.ToList();
         }
+
+        public RedirectToPageResult OnPost()
+        {
+            todo.Status = "Incomplete";
+            _db.Add<Todo>(todo);
+            _db.SaveChangesAsync();
+            return RedirectToPage();
+        }
+
     }
 }
